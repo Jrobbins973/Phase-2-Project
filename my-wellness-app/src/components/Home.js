71238741c5f1da5b3bf5761c6a09cc12c283c901
Trey <trey.delcamp@gmail.com>
2022-10-27 14:08:24 -0500

    Couldn't resize button, BUT resized and added photo to Home

--- my-wellness-app/src/components/Home.js
+++ my-wellness-app/src/components/Home.js
@@ -2,7 +2,8 @@ import React from "react";
 
 function Home(props){
     return <div>
-        <img src={'https://www.cdc.gov/physicalactivity/basics/adults/images/Health_Benefits_PA_Adults_Jan2021_H.jpg?_=61062'}/>
+        <img style= {{height: 750, width: 750, margin: 10}} src={'https://www.cdc.gov/physicalactivity/basics/adults/images/Health_Benefits_PA_Adults_Jan2021_H.jpg?_=61062'}/>
+        <img style= {{height: 750, width: 750, margin: 10}} src={"https://uploads.prod01.sydney.platformos.com/instances/446/assets/modules/homepage/webapp_uploads/blog/images/stretching1635409828919.png"}/>
         </div>
 }
 
